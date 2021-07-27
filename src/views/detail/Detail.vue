44b68db22d75dd5692c6057e5b785df2e7545892
jj <2380228977@qq.com>
2021-07-27 20:25:06 +0800

    '详情页面：轮播图和商品基础信息展示'

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,26 +1,49 @@
 <template>
   <div id="detail">
     <detail-nar-bar></detail-nar-bar>
+    <detail-swiper :top-images="topImages"></detail-swiper>
+    <detail-base-info :goods="goods"></detail-base-info>
   </div>
 </template>
 
 <script>
 
 import DetailNarBar from "./childComps/DetailNarBar";
+import DetailSwiper from "./childComps/DetailSwiper";
+import DetailBaseInfo from "./childComps/DetailBaseInfo";
+
+import {getDetail,Goods} from 'network/detail';
 
 export default {
   name: "Detail",
   data(){
     return {
-      iid: null
+      iid: null,
+      topImages: [],
+      goods: {}
     }
   },
   components: {
-    DetailNarBar
+    DetailNarBar,
+    DetailSwiper,
+    DetailBaseInfo
+  },
+  methods: {
+    getDetailData(){
+      this.iid = this.$route.query.iid;
+      getDetail(this.iid)
+        .then(res => {
+          console.log(res);
+          const data = res.result
+          // console.log(res.result.itemInfo.topImages);
+          this.topImages = data.itemInfo.topImages;
+          this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services);
+        })
+    }
   },
   created() {
     console.log(this.$route.query.iid);
-    this.iid = this.$route.query.iid;
+    this.getDetailData();
   }
 }
 </script>
---
+++ src/views/detail/childComps/DetailBaseInfo.vue
@@ -0,0 +1,99 @@
+<template>
+  <div v-if="Object.keys(goods).length !== 0" class="info">
+    <div class="info-title">{{goods.title}}</div>
+    <div class="info-price">
+      <span class="price-newPrice">{{goods.newPrice}}</span>
+      <span class="price-oldPrice">{{goods.oldPrice}}</span>
+      <span class="price-discount">{{goods.discount}}</span>
+    </div>
+    <div class="info-other">
+      <div>{{goods.columns[0]}}</div>
+      <div>{{goods.columns[1]}}</div>
+      <div>{{goods.services[goods.services.length - 1].name}}</div>
+    </div>
+    <div class="info-services">
+      <div v-if="goods" v-for="index in (goods.services.length - 1)" :key="index">
+        <img :src="goods.services[index-1].icon" alt="">
+        <span>{{goods.services[index-1].name}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailBaseInfo",
+  props: {
+    goods: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.info{
+  padding: 0 10px;
+  margin: 15px auto;
+  color: var(--color-text);
+}
+
+.info-title{
+  font-size: 20px;
+  font-weight: bold;
+  margin: 15px auto;
+
+}
+
+.info-other{
+  display: flex;
+  justify-content: space-between;
+  font-size: 14px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #e9e9e9;
+  margin: 25px auto;
+}
+
+.info-price{
+  position: relative;
+}
+
+.price-newPrice{
+  color: var(--color-tint);
+  font-size: 30px;
+  margin-right: 5px;
+}
+
+.price-oldPrice{
+  font-size: 14px;
+  margin-right: 5px;
+  text-decoration: line-through;
+}
+
+.price-discount{
+  position: absolute;
+  top: 0px;
+  /*display: block;*/
+  background-color: var(--color-tint);
+  color: #ffffff;
+  font-size: 14px;
+  border-radius: 10px;
+  padding: 3px;
+  /*margin-top: -10px;*/
+}
+
+.info-services{
+  display: flex;
+  justify-content: space-between;
+  font-size: 10px;
+}
+
+.info-services img{
+  width: 10px;
+  height: 10px;
+  vertical-align: center;
+}
+</style>
